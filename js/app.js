6350d5e255bf18eb2847297c6dafcd32cf588edb
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-02-27 20:58:07 -0600

    palindrome finished but got a LOT of help

--- js/app.js
+++ js/app.js
@@ -11,4 +11,17 @@ console.log("something is definitely happening");
 	//and make sure everythigs working coreclty
 	// return is what WILL actually happen. it also ends the function.
 
-	fghghgh
+// Palindromes
+
+const ifItIsPalindrome = (str) => { // set up the funk
+	const lowerstr = str.toLowerCase() // from MDN -- ](str.toLowerCase()); -- The toLowerCase() 
+	//method returns the calling string value converted to lower case.
+	const lenstr = lowerstr.length
+	const lenMid = Math.floor(lenstr / 2) // need to iterate over broken string (divide by 2 breaks in half)
+	for (let i = 0; i < lenMid; i++) {
+		if (lowerstr[i] !== lowerstr[lenstr -1 -i ]) { // strick compairison for string's index [i] with strings length (minus 1)
+			return false // it it doesnt, then return false
+		} return true // but if it does, then yes.
+	} 
+} 
+console.log(ifItIsPalindrome("racecar"))
