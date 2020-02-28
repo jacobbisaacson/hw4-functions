ba2cdb07d2de88f327006242afbb17e24a462b47
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-02-27 20:49:56 -0600

    omg im so sorry

---
+++ js/app.js
@@ -0,0 +1,14 @@
+console.log("something is definitely happening");
+
+//What is the difference between a parameter and an argument?
+	// PARAMETER is the input into the function, which can change the behavior of the function
+	// const functionName = (PARAMETER) => { some code that will work with the parameter }
+	// ARGUMENT is when you call a function and tell it what the va;ue will be for its parameter, 
+	// so giving the function an argument.  PASSING in an argument and CALLING a function
+	// functionName(ARGUMENT)
+//Within a function, what is the difference between return and console.log?
+	// console log is more to help ourselves learn what will acutally happen with our code, so we can see our progress,
+	//and make sure everythigs working coreclty
+	// return is what WILL actually happen. it also ends the function.
+
+	fghghgh
